0bee2049a85cbaa160be601703aab409198362f2
Julia <jlopeza2020@alumnos.urjc.es>
2024-03-18 21:02:12 +0100

    working version pinhole

--- code/camera/pinhole.py
+++ code/camera/pinhole.py
@@ -1,43 +1,63 @@
 
-
-class Point3D:
-    def __init__(self, x, y, z):
-        self.x = x
-        self.y = y
-        self.z = z
-
-class Point2D:
-    def __init__(self, x, y, z):
-        self.x = x
-        self.y = y
-        self.z = 1
+import numpy as np
+import cv2
+import math 
 
 # de 2d a 3d
-def get3Dpoint(Point 2D):
+# def get3Dpoint(point2d, K):
 
-    x = ((x-cx)*d)/fx
-    y  = ((y -cy)*d)/ fy
+#     point2d_x = point2d[0]
+#     point2d_y = point2d[1]
 
-    z = depth
+#     cx = K[][]
+#     cy = K[][]
+
+#     fx = K[][]
+#     fy = K[][]
 
 
-    return 3dpoint
+#     x = ((point2d_x-cx)*d)/fx
+#     y  = ((point2d_y -cy)*d)/ fy
 
+#     z = depth
+
+    
+
+#     return 
+
+def getradian(degrees):
+
+    return ((degrees*math.pi)/180)
 # de 3d a 2d 
-def get2Dpoint(Point 3D):
+def get2Dpoint(K, point3d, degreevalue):
 
-    K = np.array([[333.76, 0.0, 310.99],
+       
+    rad = getradian(degreevalue)
+
+    #K = np.array([[333.76, 0.0, 310.99],
+    #              [0.0, 335.08, 230.93],
+    #              [0.0, 0.0, 1.0]])
+    RT = np.array([[np.cos(rad), 0.0, np.sin(rad),     0.0],
+                [    0.0 , 1.0,       0.0,     0.0],
+                [-np.sin(rad), 0, np.cos(rad), 110.0]])
+    
+    aux = RT.dot(point3d)
+
+    return K.dot(aux)
+    
+
+K = np.array([[333.76, 0.0, 310.99],
                   [0.0, 335.08, 230.93],
                   [0.0, 0.0, 1.0]])
-    RT = np.array([[cos(40), 0, sin(40),     0],
-                [    0 , 1,       0,     0],
-                [-sin(40), 0, cos(40), 110]])
-    2d = K*RT
-    
-    return 2d
 
+point3d = np.array([1.0,0.0,0.0,1.0])
 
+point2d = get2Dpoint(K, point3d, 40)
 
+print(point2d)
+
+finalpoint2D = np.array([ point2d[0]/point2d[2], point2d[1]/point2d[2]])
+print(finalpoint2D)
 
 
 # parámetros extrínsecos 
