5158ea1722d7235b52658563c6d47b73811e5e5e
julia <jlopeza2020@alumnos.urjc.es>
2024-06-12 17:33:24 +0200

    v1 of HRI using testing_motorsv3.py

---
+++ code/control/procesa.php
@@ -0,0 +1,23 @@
+<?php
+$valor_estado=$_POST['valor_estado'];
+switch ($valor_estado) {
+	case 1:
+		exec('sudo python /var/www/html/control/py/ahead.py');
+		break;
+	case 2:
+		exec('sudo python /var/www/html/control/py/left.py');
+		break;
+	case 3:
+		exec('sudo python /var/www/html/control/py/right.py');
+		break;
+	case 4:
+		exec('sudo python /var/www/html/control/py/back.py');
+		break;
+	case 5:
+		exec('sudo python /var/www/html/control/py/stop.py');
+		break;
+	default:
+		exec('sudo python /var/www/html/control/py/stop.py');
+		break;
+}
+?>
---
+++ code/control/py/ahead.py
@@ -0,0 +1,34 @@
+import RPi.GPIO as GPIO
+import time
+
+#Set function to calculate percent from angle
+def angle_to_percent (angle) :
+    if angle > 180 or angle < 0 :
+        return False
+
+    start = 4
+    end = 12.5
+    ratio = (end - start)/180 #Calcul ratio from angle to percent
+
+    angle_as_percent = angle * ratio
+
+    return start + angle_as_percent
+
+GPIO.setmode(GPIO.BOARD) #Use Board numerotation mode
+GPIO.setwarnings(False) #Disable warnings
+
+#Use pin 4 for PWM signal
+pwm_gpio_left = 7
+#Use pin 18 for PWM signal
+pwm_gpio_right = 12
+frequence = 50
+
+GPIO.setup(pwm_gpio_left, GPIO.OUT)
+pwm_left = GPIO.PWM(pwm_gpio_left, frequence)
+GPIO.setup(pwm_gpio_right, GPIO.OUT)
+pwm_right = GPIO.PWM(pwm_gpio_right, frequence)
+
+#Go ahead
+pwm_left.start(angle_to_percent(180))
+pwm_right.start(angle_to_percent(0))
+time.sleep(1)
