22da8ed5955feb1ea6edb54993b1bf1b7bfbf4a9
julia <jlopeza2020@alumnos.urjc.es>
2024-06-23 23:00:45 +0200

    modified gps_send.py

--- code/gps/gps_send.py
+++ code/gps/gps_send.py
@@ -2,29 +2,22 @@ import pyrebase
 import serial
 import pynmea2
 
-firebaseConfig={"apiKey": "AIzaSyBgrLRMuPB8bkaMUNg4XlQKTbY34JgWwic",
-
-  "authDomain": "gps-tracker-c5d0e.firebaseapp.com",
-
-  "databaseURL": "https://gps-tracker-c5d0e-default-rtdb.europe-west1.firebasedatabase.app",
-
-  "projectId": "gps-tracker-c5d0e",
-
-  "storageBucket": "gps-tracker-c5d0e.appspot.com",
-
-  "messagingSenderId": "443475027920",
-
-  "appId": "1:443475027920:web:dc2a4bf0259b861f3d7bd5",
-
-  "measurementId": "G-6JPHY9G9S1"
-
+firebaseConfig={
+    "apiKey": "AIzaSyBgrLRMuPB8bkaMUNg4XlQKTbY34JgWwic",
+    "authDomain": "gps-tracker-c5d0e.firebaseapp.com",
+    "databaseURL": "https://gps-tracker-c5d0e-default-rtdb.europe-west1.firebasedatabase.app",
+    "projectId": "gps-tracker-c5d0e",
+    "storageBucket": "gps-tracker-c5d0e.appspot.com",
+    "messagingSenderId": "443475027920",
+    "appId": "1:443475027920:web:dc2a4bf0259b861f3d7bd5",
+    "measurementId": "G-6JPHY9G9S1"
     }
 
 firebase=pyrebase.initialize_app(firebaseConfig)
 db=firebase.database()
 
 while True:
-        port="/dev/ttyAMA0"
+        port="/dev/ttyS0"
         ser=serial.Serial(port, baudrate=9600, timeout=0.5)
         dataout = pynmea2.NMEAStreamReader()
         newdata=ser.readline()
