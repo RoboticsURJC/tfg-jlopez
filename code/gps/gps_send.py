5feef9b036cba7fae8ce9d6794d8fefab6734e85
Julia <jlopeza2020@alumnos.urjc.es>
2024-06-24 13:22:07 +0200

    wip real tracking

--- code/gps/gps_send.py
+++ code/gps/gps_send.py
@@ -17,18 +17,20 @@ firebase=pyrebase.initialize_app(firebaseConfig)
 db=firebase.database()
 
 while True:
-        port="/dev/ttyS0"
-        ser=serial.Serial(port, baudrate=9600, timeout=0.5)
-        dataout = pynmea2.NMEAStreamReader()
-        newdata=ser.readline()
-        n_data = newdata.decode('latin-1')
-        if n_data[0:6] == '$GPRMC':
-                newmsg=pynmea2.parse(n_data)
-                lat=newmsg.latitude
-                lng=newmsg.longitude
-                gps = "Latitude=" + str(lat) + " and Longitude=" + str(lng)
-                print(gps)
-                data = {"LAT": lat, "LNG": lng}
-                db.update(data)
-                print("Data sent")
-        # tengo que añadir lo de parse error
+    port="/dev/ttyS0"
+    ser=serial.Serial(port, baudrate=9600, timeout=0.5)
+    dataout = pynmea2.NMEAStreamReader()
+    newdata=ser.readline()
+    n_data = newdata.decode('latin-1')
+    if n_data[0:6] == '$GPRMC':
+        try:
+            newmsg=pynmea2.parse(n_data)
+            lat=newmsg.latitude
+            lng=newmsg.longitude
+            gps = "Latitude=" + str(lat) + " and Longitude=" + str(lng)
+            print(gps)
+            data = {"LAT": lat, "LNG": lng}
+            db.update(data)
+            print("Data sent")
+        except pynmea2.ParseError:
+            pass
