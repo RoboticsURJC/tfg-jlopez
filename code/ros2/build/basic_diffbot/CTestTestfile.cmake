57be25fd4964c96b11b88ec1df868ac18147c649
Julia <jlopeza2020@alumnos.urjc.es>
2024-08-05 19:01:34 +0200

    working base gazebo

---
+++ code/ros2/build/basic_diffbot/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/juloau/Desktop/TFG/tfg-jlopez/code/ros2/src/basic_diffbot
+# Build directory: /home/juloau/Desktop/TFG/tfg-jlopez/code/ros2/build/basic_diffbot
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
--- code/ros2/build/pibotj_urdf_complete/CMakeFiles/pibotj_urdf_complete_uninstall.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/pibotj_urdf_complete_uninstall"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/pibotj_urdf_complete_uninstall.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
